0b863a1c802fc52f698db9f150daa83352efeb0f
nands <ferdi@nand.co.za>
2012-09-22 12:58:06 +0200

    Dammit, I should learn to look for debug output. :\

--- static/scripts/security.js
+++ static/scripts/security.js
@@ -5,7 +5,6 @@ var security = {
 				val = parseInt(($('input[name=username]').val().length + $('input[name=email]').val().length), 10);
 
 			$('input[name=verify]').val(val);
-			console.log(val);
 		};
 
 		$('input[name=username]').keyup(function(){
